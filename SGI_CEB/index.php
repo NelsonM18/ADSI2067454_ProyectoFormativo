854af88c7c781fd1f5ed6a7fb346152e916a56e9
david20842 <d-hasseljoff8@outlook.com>
2021-02-01 15:39:41 -0500

    Correccion Carpetas
    Docs: Se añaden las carpetas.

---
+++ SGI_CEB/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>SGI CEB</title>
+	<link rel="stylesheet" type="text/css" href="./css/styles.css"/>
+</head>
+<body>
+
+	<header>
+		<div class="contenedor"> 
+			<a href="index.php"><h1>SGI CEB</h1></a>
+			<input type="checkbox" id="menu-roll">
+			<label for="menu-roll"><img src="./img/menu-roll.png" alt=""></label>
+			<nav class="menu">
+				<ul>
+					<a href=""><li>Inicio</li></a>
+					<a href=""><li>Circulares Escolares</li></a>
+					<a href=""><li>Retardos Estudiantiles</li></a>
+					<a href=""><li>¿Quiénes somos?</li></a>
+					<a href="ingresar.php"><li>Ingresar</li></a>
+				</ul>
+			</nav>
+		</div>
+	</header>
+
+	<div class="contenedor" id="contenedor_chingon">
+	<section id="seccion_inicio">
+		
+			<p>SISTEMA DE GESTIÓN DE INGRESO</p>
+			<p>CIUDADELA EDUCATIVA DE BOSA</p>
+			<img src="img/colegio.jpg" alt="">
+		
+	</section>
+
+	<aside id="seccion_Iaside">
+		
+			<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta quibusdam eligendi ipsum! Facere delectus asperiores harum fuga totam aspernatur odio? Praesentium doloremque debitis tempore quo exercitationem inventore quis officiis sequi?</p>
+			
+	</aside>
+	</div>
+
+	
+	<footer id="pie">
+		<div class="contenedor">
+			<p>Desarrollado por SGI &copy 2021</p>
+		</div>
+	</footer>
+
+</body>
+</html>
